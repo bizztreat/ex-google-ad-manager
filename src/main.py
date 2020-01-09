4f585a8b0fb8690f9b5519271512c7e443670444
Tomáš Votava <tomas.votava@bizztreat.com>
2020-01-09 19:00:54 +0100

    Code review

--- src/main.py
+++ src/main.py
@@ -1,25 +1,40 @@
 #!/usr/bin/env python
 import os
+import sys
 import json
 
 from googleads import ad_manager
 from googleads import oauth2
 
+def main():
+    """Main function encapsulating everything this extractor does
+    """
+    config_path = sys.argv[1] if len(sys.argv) == 2 else "/data/config.json"
 
-service_path = "../service.json"
+    if not os.path.exists(config_path):
+        raise Exception("Configuration not specified, was expected at '{}'".format(config_path))
 
-if not os.path.exists(service_path):
-    raise Exception("Configuration not specified")
+    with open(config_path, encoding="utf-8") as conf_file:
+        conf = json.load(conf_file)["parameters"]
 
-# OAuth2 credential info
-key_file = service_path
+    ## Store credentials in a file
+    with open("/tmp/service.json", "w", encoding="utf-8") as service_file:
+        json.dump(conf["service_account"], service_file)
 
-oauth2_client = oauth2.GoogleServiceAccountClient(
-    key_file, oauth2.GetAPIScope('ad_manager'))
+    # OAuth2 credential info
+    key_file = "/tmp/service.json"
 
-ad_manager_client = ad_manager.AdManagerClient(
-      oauth2_client, "ex-bizztreat-google-ad-manager")
+    oauth2_client = oauth2.GoogleServiceAccountClient(
+        key_file,
+        oauth2.GetAPIScope('ad_manager')
+        )
 
-print(ad_manager_client.GetService("NetworkService").getAllNetworks())
+    ad_manager_client = ad_manager.AdManagerClient(
+        oauth2_client,
+        "ex-bizztreat-google-ad-manager"
+        )
 
+    print(ad_manager_client.GetService("NetworkService").getAllNetworks())
 
+if __name__ == "__main__":
+    main()
