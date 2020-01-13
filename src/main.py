e3b0200ee2d6b22743eaebe50e68a4fc9d1dd413
Tomáš Votava <tomas.votava@bizztreat.com>
2020-01-13 15:22:47 +0100

    ReportDownloader

--- src/main.py
+++ src/main.py
@@ -2,9 +2,11 @@
 import os
 import sys
 import json
+import gzip
 
 from googleads import ad_manager
 from googleads import oauth2
+
 
 def main():
     """Main function encapsulating everything this extractor does
@@ -12,12 +14,13 @@ def main():
     config_path = sys.argv[1] if len(sys.argv) == 2 else "/data/config.json"
 
     if not os.path.exists(config_path):
-        raise Exception("Configuration not specified, was expected at '{}'".format(config_path))
+        raise Exception(
+            "Configuration not specified, was expected at '{}'".format(config_path))
 
     with open(config_path, encoding="utf-8") as conf_file:
         conf = json.load(conf_file)["parameters"]
 
-    ## Store credentials in a file
+    # Store credentials in a file
     with open("./service.json", "w", encoding="utf-8") as service_file:
         json.dump(conf["#service_account"], service_file)
 
@@ -27,14 +30,31 @@ def main():
     oauth2_client = oauth2.GoogleServiceAccountClient(
         key_file,
         oauth2.GetAPIScope('ad_manager')
-        )
+    )
 
     ad_manager_client = ad_manager.AdManagerClient(
         oauth2_client,
-        "ex-bizztreat-google-ad-manager"
-        )
+        "ex-bizztreat-google-ad-manager",
+        network_code=conf["network_code"]
+    )
 
-    print(ad_manager_client.GetService("NetworkService").getAllNetworks())
+    downloader = ad_manager_client.GetDataDownloader(version="v201911")
+
+    report_job = {"reportQuery": {
+        "dimensions": ["ADVERTISER_NAME", "COUNTRY_NAME"],
+        "columns": ["AD_SERVER_CLICKS"],
+        "dateRangeType": "LAST_3_MONTHS"
+    }
+    }
+
+    job = downloader.WaitForReport(report_job)
+
+    with open("./output/output.csv.gz", "wb") as fid:
+        downloader.DownloadReportToFile(
+            job,
+            "CSV_DUMP",
+            fid
+        )
 
 if __name__ == "__main__":
     main()
